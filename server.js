e10964f7a9106110c579ad04ebeaf8885f695716
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-07-10 17:11:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
-const { notes } = require('./Develop/db/db.json')
-
+const notes = require('./Develop/db/db.json')
+const path = require('path');
+const uuid = require('uuid');
 const express = require('express');
 const app = express();
 
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(port);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//reach public files
 app.get('/api/notes', (req, res) => {
-    res.send('Hello!');
-    res.send('Bonjor')
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+    
+});
+//add new to db.json
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const notesNew = req.body;
+    notesNew.id = uuid.v4();
+    notes.push(notesNew);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
 });
 
-app.listen(3004, () => {
-    console.log(`API server now on port 3002!`);
-});
+//delete notes
+app.delete('/api/notes/:id', (req, res) => {
+const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+const dispatchNote = notes.filter((delNote) => delNote.id !== req.params.id);
+fs.writeFileSync('.db/db.json', JSON.stringify(dispatchNote));
+res.json(dispatchNote);
+})
+
+//html call home
+
+
